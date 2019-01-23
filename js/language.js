fecef33ad78cd804d07896183165c395afde3e95
intrafuse <sam@intrafuse.com>
2019-01-22 22:41:18 -0800

    push

---
+++ js/language.js
@@ -0,0 +1,134 @@
+	function getLanguage(lc){
+
+				$.post("./php/get_native_language.php", {language_code: lc,}, 
+				   function(result){
+                    
+					//itereate through JSON object and update UI
+					//$('#quantity-label').html('testing');
+					
+					//var obj = JSON.parse(result);
+                            var j = 0;
+                            var langDocument = JSON.parse(result);
+                            var tags = document.querySelectorAll('div,input,span,a,label,option,textarea,select,button');
+					
+                            Array.from(tags).forEach(function(value, index){
+                            var key = value.dataset.languagekey;
+                                
+                                //if(value.tagName == "DIV"){
+                                     //console.log(value.tagName);
+                                     //console.log(value.clientWidth);
+                                //}
+                                
+                            //console.log("--->" + langDocument[key]);
+                            j++;
+                            //langDocument['is-left-to-right']
+                                //console.log("*******************" + value.style.display);
+                            if(langDocument[key]){
+                                //console.log("---> (" + (lc) + ") " + langDocument['is-left-to-right']);
+                                if(key == 'captcha'){
+                                        $('select[id=captcha] option:first').html(langDocument[key]).attr("selected",true);
+                                    }else if (key == "message"){
+                                        value.placeholder = langDocument[key];
+                                    }else if (key == "total"){
+                                        labelTotal = langDocument[key];
+                                    }else if (key == "state"){
+                                         value.placeholder = langDocument[key];
+                                    }else{
+                                        
+                                    let e = measureString(langDocument[key]);
+                                         //value.style.width = "300px";
+                                    if(e > value.clientWidth){
+                                        
+                                        //resizeText(langDocument[key],e,value.id);                                 
+                                        //console.log(value.id + ": " + value.clientWidth);
+                                        //console.log("font size: " + window.getComputedStyle(document.getElementById(value.id)).fontSize);
+                                        //value.style.fontSize = "22px";
+                                        //resizedFont = resizeText(langDocument[key],e,value.clientWidth);
+                                        //value.style.width = "300px"; //value.clientWidth + 20 + "px";
+                                        //value.style.fontSize = resizedFont;
+                                        //console.log(resizedFont);
+                                    }
+                                        
+                                        value.placeholder = langDocument[key];
+                                        value.innerText =  decodeEntities(langDocument[key]);
+                                }
+                            }
+                                $('#language-picker').hide();
+						});
+                        //$(".nav-button").fitText(.1, { minFontSize: '8px', maxFontSize: '50px' });
+                        //$().width(); 
+                        //font: normal normal 25px/50px 'Comfortaa', cursive;
+						$("#back-button-label").boxfit({align_center:true,align_middle:true});
+						$("#next-button-label").boxfit({align_center:true,align_middle:true});
+						$("#order-form-subheading-1").boxfit({align_center:true,maximum_font_size:35});
+                        //Initialize total value 0
+                    
+                        
+                    
+                        var locale = languageCode + "-" + countryCode;
+                        var formattedCurrency = formatCurrency(currencyCode,locale,0);		
+                        $('#total').html(labelTotal + ": " + formattedCurrency); 			
+                        //$('#review-order-price').html(labelTotal + ": " + formattedCurrency); 
+                        convert(currencyCode,"stripe");
+   
+				}
+			);
+	}
+
+
+
+        
+	function detectLanguage(override = false){
+  
+			var getlcParam = getLCfromQS();
+
+			if(getlcParam.length > 0){
+				languageCode = getlcParam;
+                getLanguage(languageCode);
+                return;
+			}
+        
+		$.get("https://krane.tv/php/language.php", function (response) {
+
+			//#############################################
+			//#############################################
+			//#############################################
+			//Fallback languages
+			//#############################################
+			//#############################################
+			//#############################################
+			//TODO: possible MySQL/server-side solution?
+
+			if(response == "mo"){
+				response = "ro";
+			}
+
+			console.log("LANGUAGE CODE = " + response);
+			//if(isDesbug().length > 0){response = isDesbug()};
+			languageCode = response; 
+			getLanguage(languageCode);
+
+		});	
+
+	}
+
+
+
+function languageSearch(all = false){
+        var qstring;
+         qstring = all ? "" : $("#language-search").val();
+        $.get("./php/language_dropdown_list.php", {qs: qstring}, function(result){
+        if(result){
+            z = document.getElementById('countries2');
+            z.innerHTML = "";
+            X = $.parseJSON(result);
+             $.each(X, function(i, obj) {
+                 $("#countries2").append("<li id = 'countryID'  onclick = \"getLanguage('" + obj.language_code + "');document.getElementById('language-label').innerHTML = '" + obj.language_name_translated + "';\">" + obj.language_name_translated + "</li>");
+            })                 
+        }
+    })
+    
+}
+
+
+
---
+++ js/stripe.js
@@ -0,0 +1,339 @@
+ 
+   
+
+
+
+		var style = {
+		  base: {
+			iconColor: '#666EE8',
+			color: '#31325F',
+			lineHeight: '40px',
+			fontWeight: 300,
+			letterSpacing: '.05em',
+			fontFamily: 'Arial',
+			fontSize: '15px',
+
+			'::placeholder': {
+			  color: '#000000',
+			},
+		  },
+		};
+
+
+
+
+var stripe = Stripe('pk_test_fgP1IfitRDyrHERTzo9BABmR');
+        var elements = stripe.elements();
+        var z = 1;
+
+
+    function manuallyEnteredCard(result){
+		
+		var successElement = document.querySelector('.success');
+		var errorElement = document.querySelector('.error');
+		
+		successElement.classList.remove('visible');
+		errorElement.classList.remove('visible');
+		
+		if(result.token){
+				//A token was generated successfully
+				console.log("Token generated successfully::: " + result.token.id);
+                saveTokenToSession(result.token.id);
+                $("#cc-inputs").animate({left: '-600px'});
+                $("#CC-review-order").animate({left: '0px'});
+                $("#CC-review-order").css('display', 'block');
+                $("#main").css('visibility', 'hidden');
+                return true;			
+			}else if (result.error) {
+				counter2 = 5;
+				$('#next').show();
+				$('#next-button-label').show();
+				errorElement.textContent = result.error.message;
+				errorElement.classList.add('visible');
+				return false;
+		}
+	}
+
+	var paymentRequest;
+	function generatePaymentRequest(){
+        
+		if(prButton){
+			prButton.destroy();
+		};
+	
+		//If currency is non-decimal, change multiplier to 1
+		if(currencyCode == "jpy"){
+				multiplier = 1;
+			}else{
+				multiplier = 100;
+		}
+        
+        $.post("./php/save_customer_information.php", {
+            "updateType": "cart",
+            "cart": JSON.stringify(cart)
+        }, function(result){
+            if(result > 0){
+                    return true;
+                }else{
+                    return false;     
+            }
+        });
+        
+        console.log("payment request: " + calculatedTotal);
+		paymentRequest = stripe.paymentRequest({
+			country: 'US',
+			currency: currencyCode,
+			total: {
+			label: 'Total: ',
+			amount: calculatedTotal,
+			},
+			requestShipping: true,
+			requestPayerName: true,
+			requestPayerEmail: true,
+            /*// `shippingOptions` is optional at this point:
+            shippingOptions: [
+            // The first shipping option in this list appears as the default
+            // option in the browser payment interface.
+            {
+              id: 'shipping',
+              label: 'Shipping Included',
+              detail: 'Arrives in 5 to 7 days',
+              amount: 500,
+            },
+            {
+              id: 'shipping 2',
+              label: 'Shipping outside US',
+              detail: '',
+              amount: 20000,
+            },
+            ],*/
+		});
+
+		//re-create stripe payment request button with new values 
+		prButton = elements.create('paymentRequestButton', {
+		  paymentRequest: paymentRequest,
+		 style: {
+			paymentRequestButton: {
+			  type: 'default',
+			  theme: 'light-outline',
+			  height: '40px',
+			},
+		  },
+		})	
+
+		paymentRequest.on('token', function(ev) {
+            console.log("paymentRequest.on('token')");
+            //console.log("--->" + ev.error.message);
+            console.log("__________________source " + ev.payerEmail);
+            console.log("__________________source " + ev.payerName);
+			saveTokenToSession2(ev);
+		});
+        
+        paymentRequest.on('shippingaddresschange', function(ev) {
+          if (ev.shippingAddress.country !== 'US') {
+                  ev.updateWith({
+                        total: {
+                        amount : calculateShipping("international") + calculatedTotal,
+                        label: "Total: "
+                           },
+                    displayItems: cartDisplayItems(),
+                    status: 'success',
+                    shippingOptions: [
+			// The first shipping option in this list appears as the default
+			// option in the browser payment interface.
+			{
+			  id: 'shipping',
+			  label: 'International Shipping',
+			  detail: 'Shipping is flat-rate for international orders',
+			  amount: 2000,
+			}]});
+              //ev.updateWith({status: 'invalid_shipping_address'}); 
+          } else {
+              /*            // Perform server-side request to fetch shipping options
+            fetch('/calculateShipping', {
+                data: JSON.stringify({
+                shippingAddress: ev.shippingAddress
+                })
+            }).then(function(response) {
+                return response.json();
+            }).then(function(result) {
+                ev.updateWith({
+                    status: 'success',
+                    shippingOptions: result.supportedShippingOptions,
+                });
+            });*/
+              
+/*                ev.updateWith({
+                    status: 'success',
+                    total: {
+                        amount : 100,
+                        label: "heloz"
+                           }
+                }
+                );*/
+              console.log("))) " + cartItem[0]);
+              
+                ev.updateWith({
+                    status: 'success',
+                        total: {
+                        amount : calculateShipping("US") + calculatedTotal,
+                        label: "Total: "
+                           },
+                    displayItems: cartDisplayItems(),
+                    shippingOptions: [
+			{
+			  id: 'shipping',
+			  label: 'Shipping',
+			  detail: 'Arrives in 5 to 7 days',
+			  amount: 500 * z,
+			}] 
+            });
+              console.log("shipping within US");
+          }
+        });
+	}
+
+
+function cartDisplayItems(){
+    
+          //{amount: calculateShipping("US"), label: "Shipping x " + cart["k1"],}
+            cartDisplayItemsArr = [];
+            for(x in cartItem){
+                if(cart["k" + x] > 0){
+                    cartDisplayItemsArr[x] = {
+                                amount: ((cartItem[x] * 1000 / 10) * cart["k" + x]), 
+                                label: "Krane x (" + cart["k"+x] + " @ " + cartItem[x] + " each)"
+                    }
+                }
+            }
+
+    return cartDisplayItemsArr;
+}
+
+	function saveTokenToSession2(ev){
+        console.log("saveTokenToSession2 called_____ " + ev.token.id);
+		$.post("./php/checkout_WPA.php", {"saveToken": ev.token.id}, function(result){
+                    if(result){
+                            $("#main").css('visibility', 'hidden');
+                            digitalWalletCheckout(ev);
+                        }else{
+                            //generate error
+                    }
+		});
+	}
+
+	function saveTokenToSession(token){
+        console.log("saveTokenToSession called");
+		$.post("./php/checkout.php", {"saveToken": token}, function(result){
+                    if(result){
+                            fillHoles(11);
+                            $("#cc-inputs").animate({left: '-600px'});
+                            $("#CC-review-order").animate({left: '0px'});
+                            $("#main").css('visibility', 'hidden');
+                        }else{
+                            //generate error
+                    }
+		});
+	}
+
+	function digitalWalletCheckout(ev){
+		//ev is passed from payments API
+		//calculatedAmount = ttl * multiplier;
+		$.post("./php/checkout_WPA.php", {"checkout": "true", 
+                                      "amount": calculatedAmount, 
+                                      "currency": currencyCode, 
+                                      "email" : ev.payerEmail,
+                                      "name" : ev.payerName
+                                     }, function(result){
+			//console.log("amount: " + calculatedAmount + " currencyCode: " + currencyCode + " result: " + result + "<<<");
+            if(result == "true"){
+                    console.log("web payments API");
+                    $("#main").fadeTo(250,0.0);
+                    $("#CC-review-order").hide();
+                    $("#pay-with").hide();
+                    $("#order-complete").show();
+                    $("#order-complete").animate({left: '0px'});
+                    ev.complete('success');	         
+				}else{
+                    console.log("checkout failed");
+                    ev.complete('fail');
+			}
+		});
+	}
+
+	function checkout(){
+		//ev is passed from payments API
+		//calculatedAmount = ttl * multiplier;
+        $("#checkout-spinner").fadeIn(250);
+        $("#review-container").fadeTo(250,0.3);
+		$.post("./php/checkout.php", {"checkout": "true", "amount": calculatedTotal, "currency": currencyCode}, function(result){
+			console.log("amount: " + calculatedTotal + " currencyCode: " + currencyCode + " result: " + result + "<<<");
+            if(result){
+                    console.log("Manually entered CC info");
+                    $("#review-container").animate({left: '-600px'});
+                    $("#review-container").fadeTo(250,0.0);
+                    $("#order-complete").fadeIn(250);
+                    $("#order-complete").animate({left: '0px'});
+                    console.log("res: " + result);
+				}else{
+                    console.log("checkout failed");
+			}
+		});
+	}
+
+    function createStripeToken(){
+        stripe.createToken(cardNumberElement).then(manuallyEnteredCard);
+    }
+
+    function isWebAPIavaiable(){
+            // Check the availability of the Payment Request API first.
+            paymentRequest.canMakePayment().then(function(result) {
+                if(result){
+                    prButton.mount('#payment-request-button');
+                    paymentAPIavailable = true;
+                    moveNavPayAPI();
+                }else{ 
+                    paymentAPIavailable = false;
+                    $("#payment-request-api-container").hide();
+                    moveNavPayAPI();
+                }
+            });
+    }
+
+    function moveNavPayAPI(){
+        if(paymentAPIavailable){
+                $("#main").animate({top: '-240px',left: '-20px'});
+            }else{
+                $("#main").animate({top: '-300px',left: '-20px'}); 
+        }
+    }
+
+
+$(document).ready(function () {
+
+		cardNumberElement = elements.create('cardNumber', {
+		  style: style
+		});
+    
+		cardNumberElement.mount('#card-number-element'); 
+
+		var cardExpiryElement = elements.create('cardExpiry', {
+		  style: style
+		});
+		cardExpiryElement.mount('#card-expiry-element');
+
+		var cardCvcElement = elements.create('cardCvc', {
+		  style: style
+		});
+		cardCvcElement.mount('#card-cvc-element');
+
+		cardNumberElement.on('change', function(event) {
+			// Switch brand logo
+			if (event.brand) {
+			setBrandIcon(event.brand);
+		  }
+			manuallyEnteredCard(event);
+		});
+})
+    
+    
