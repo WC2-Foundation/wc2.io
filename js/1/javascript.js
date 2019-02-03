25a18cd8de4145edb76af6ade0655180542c15b1
intrafuse <sam@intrafuse.com>
2019-02-03 13:06:53 -0800

    .

---
+++ js/1/javascript.js
@@ -0,0 +1,422 @@
+    //"use strict";		
+    var ipStackKey = "9d13d36c2a7e9e7bc30a79a295118ee7";
+    ////https://openexchangerates.org/api/latest.json?app_id=3e824a6d77374740bffd916fd232efd1
+	var paymentRequest, prButton;
+	var exchangeRate;
+	var ttl;
+	var pass;
+	var handled = false;
+	var handled2 = false;
+	var counter = 0;
+	var counter2 = 1;
+	var currencySymbol = "$";
+	var countryCode = "US";
+	var languageCode;
+    var langDocument;
+	var labelTotal;
+	var multiplier;
+	var stripeCurrencyCode = "usd";
+	var unitOfMeasure = "centimeters";
+	var sizeStandardKrane = 72;
+	var sizeJumboKrane = 86;
+	var lengthSymbol = " cm";
+    var checkoutComplete = false;
+    var slideContainer = 600;
+    var freeUSShippingOver = 45;
+    var freeWorldShippingOver = 15;
+    var cartBuilt = false;
+    var shippingInterArr = [];
+    var shippingUSArr = [];
+    var contactInformation = {firstName: "", lastName: "", emailAddress: ""};
+
+    var cart = [
+        {k: [0,28,0]}, 
+        {k: [0,28,0]}, 
+        {k: [0,28,0]}, 
+        {k: [0,0,0]}, 
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]},
+        {k: [0,0,0]}
+    ]
+    
+        var cartObj = {
+        cartTabs: [
+            {
+                tabName: "Standard - 28&ldquo;", cartItems: [
+                    {items: [
+                            {columns: [{title: "Krane - "},{title:"Ibycus"},{size:"Standard"},{price: "24.95"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "Krane - "},{title:"Pyramís"},{size:"Standard"},{price: "28.95"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "Krane - "},{title:"Electrum"},{size:"Standard"},{price: "32.95"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]}
+                        ]}
+                    ]
+            },
+            {
+                tabName: "Large - 34&ldquo;", cartItems: [
+                    {items: [
+                            {columns: [{title: "Krane - "},{title:"Ibycus"},{size:"Large"},{price: "28.95"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "Krane - "},{title:"Pyramís"},{size:"Large"},{price: "32.95"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "Krane - "},{title:"Electrum"},{size:"Large"},{price: "36.95"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]} 
+                        ]}
+                    ]  
+            },
+            {  
+                tabName: "Accessories", cartItems: [
+                    {
+                        items: [
+                            {columns: [{title: "Tablet Extension"},{title:""},{size: ""},{price: "9.95"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]}  
+                        ],
+                    }
+                ]  
+            },
+            {  
+                tabName: "Parts", cartItems: [
+                    {
+                        items: [
+                            {columns: [{title: "parts-rubber-tip"},{title:""},{size: ""},{price: "1.00"},{shippingInt: ["9.00"] },{shippingUS: ["1.00"] }]},
+                            {columns: [{title: "parts-rubber-donut"},{title:""},{size: ""},{price: "1.00"},{shippingInt: ["9.00"] },{shippingUS: ["1.00"] }]},
+                            {columns: [{title: "parts-strut-front"},{title:""},{size: ""},{price: "4.00"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "parts-strut-back"},{title:""},{size: ""},{price: "4.00"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "parts-dowel"},{title:""},{size: ""},{price: "1.00"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "parts-main-support"},{title:""},{size: ""},{price: "3.00"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "parts-mounting-main"},{title:""},{size: ""},{price: "1.00"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "parts-wall-anchor"},{title:""},{size: ""},{price: "3.00"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "parts-wall-anchor-screw"},{title:""},{size: ""},{price: "1.00"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "parts-phone-strap"},{title:""},{size: ""},{price: "2.00"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "parts-support-cord"},{title:""},{size: ""},{price: "2.00"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "parts-cord-lock"},{title:""},{size: ""},{price: "1.00"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]},
+                            {columns: [{title: "parts-cord-buckle"},{title:""},{size: ""},{price: "1.00"},{shippingInt: ["9.00"] },{shippingUS: ["4.00"] }]}
+                        ],
+                    }
+                ]  
+            }
+        ]
+    }
+
+        
+    var calcTotal; 
+	var containers;
+    var containerID = 0;
+    var currencyCode;
+    var paypalCurrencyCode = "usd";
+    var cardNumberElement;
+    var stopSmoking = false;
+    var thumbnailHover;
+    var calculatedTotal;
+    var shippingUS = "4.00";
+    var shippingInternational = "9.00";
+    var cartItem = [];
+	var arrayOfImages = [];
+	arrayOfImages[0] = "images/main_image_lights_off.png";
+	preload(arrayOfImages);
+
+
+
+
+
+    function showTagalongOnScroll(){
+        var scrollAmount = $(window).scrollTop();
+        if(scrollAmount > 755){
+            $("#tagalongBar-container").fadeIn(500);
+        }else{
+            $("#tagalongBar-container").hide();
+        }
+    }
+
+
+
+    //#############################################################	
+    //#############################################################	
+    //#############################################################	
+    //READY
+    //#############################################################	
+    //#############################################################	
+    //#############################################################	
+
+
+    var parts,canvas,ctx,paymentAPIavailable;
+
+
+    
+    var mg = 0;
+	$(document).ready(function () {
+
+        //languageSearch(true);
+        setOrderDetailInputs(true);
+        //$("#k1-qty").val(0);
+        var tags2 = document.querySelectorAll('div,input,option,textarea,select,button');
+
+        Array.from(tags2).forEach(function(value, index){
+            //value.tabIndex = "-1";
+        });
+
+        $.get("./php/language_dropdown_list.php", function (response) {
+            $("#language-picker").html(response);
+        });	
+
+        $("#language-search").autocomplete({
+            source:  function(request, response) {
+            $.getJSON('./php/language_dropdown_list.php', {
+                term: $("#language-search").val(),
+                language_code: languageCode
+            }, response);
+        },
+
+            minLength: 1,
+        }); 		
+
+        fillHoles(0);
+ 
+	//uncomment to test ipstack data
+	$.removeCookie("country_lookup", { path: '/' });
+	if ($.cookie("country_lookup")) {
+		console.log("cookie exists: " + $.cookie("country_lookup"));
+		countryCode = $.cookie("country_lookup");
+        $("#country").val(countryCode);
+        if(countryCode != "US"){
+                $("#state").attr("placeholder", "Province");
+        }
+		$("#countries option[value=\"" + $.cookie("country_lookup") + "\"]").attr('selected','selected');
+		detectLanguage();
+		//convert(currencyCode);
+	}else{
+			//First visit, grab language and currency data
+			$.get("https://api.ipstack.com/check?access_key=" + ipStackKey, function (response) {
+				var currencySymbol = response.currency.symbol;
+                console.log("response.country_code: " + response.country_code);
+				//ajax call to mysql, make sure currency is supported; if not fallback 
+				//to US for now (TODO: country based fallback [euros etc])
+                console.log(response.currency.code.toLowerCase());
+				currencyCode = response.currency.code.toLowerCase(); //response.currency.code.toLowerCase(); //nok
+                //TODO: fix paypal convert causes tabs to render twice etc 
+                //paypalCurrencyCode =  "usd"; //convert(currencyCode,"paypal"); //response.currency.code.toLowerCase();
+				countryCode = response.country_code;//response.country_code;
+				
+				if(countryCode == "US"){
+					unitOfMeasure = "inches";
+					sizeStandardKrane = 28;
+					sizeJumboKrane = 34;
+					lengthSymbol = "&#34;";
+				}
+                
+                
+                paypalCurrencyCode = convert(currencyCode,"paypal");
+                console.log("convert(currencyCode,paypal)::: " + paypalCurrencyCode);
+                
+				setMeasurementVariables();
+                console.log("0000000000000 " + response.location.languages[0]["code"]);
+				var lc = response.location.languages[0]["code"]; //"en";//response.location.languages[0]["code"]; //no
+                var cc = response.country_name;
+                $("#language-label").html($("#" + lc).text());
+                convert(currencyCode,"stripe"); 
+				createCookie("country_lookup",cc);
+				$("#country").val(cc);
+                
+                detectLanguage(lc);
+                
+			}, "jsonp");
+
+	}
+        		
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    window.requestAnimationFrame = requestAnimationFrame;
+
+	canvas = document.getElementById("canvas"),
+	ctx = canvas.getContext("2d");
+	canvas.height = 300; //document.body.offsetHeight;
+	canvas.width = 300;    
+    parts = [],
+    minSpawnTime = 60,
+    lastTime = new Date().getTime(),
+    maxLifeTime = Math.min(5000, (canvas.height/(1.5*60)*1000)),
+    emitterX = canvas.width / 2,
+    emitterY = canvas.height - 50,
+    smokeImage = new Image();	
+	smokeImage.src = "images/smoke.png";
+	
+	smokeImage.onload = function () {
+		render();
+	}
+
+        
+});	
+
+    showTagalongOnScroll();
+
+
+//#############################################################	
+//#############################################################	
+//#############################################################	
+//END READY
+//#############################################################	
+//#############################################################	
+//#############################################################
+
+
+        
+		function sendContactFormMessage(){ 
+                
+                $.post("./php/contact_form.php", {emailOrPhoneNumber: $("#emailOrPhoneNumber").val(), message: $("#message").val()}, function(result){
+				
+				if(result){
+						$("#contact-form-wrapper").hide();
+						$("#message-sent").show();
+						console.log("sendContactFormMessage: " + result);
+					}else{
+						$("#contact-form-wrapper").hide();
+						$("#message-sent").show();
+						$("#message-sent").text("Send Failed. Please use email address.");
+						console.log("Failed");
+				}
+			});			
+		}
+        
+
+    function measureString(str){
+        canvas = document.createElement('canvas');
+        ctx = canvas.getContext("2d");
+        fontSize = 28;
+        ctx.font = "normal normal " + fontSize + "px 'Comfortaa', cursive";       
+        width = ctx.measureText(str).width; 
+        return width;
+    }
+ 
+    function resizeDiv(str,width,ele){
+        canvas = document.createElement('canvas');
+        ctx = canvas.getContext("2d");
+        fontSize = 28;
+        while(width > ele){
+            ctx.font = "normal normal " + fontSize + "px 'Comfortaa', cursive";       
+            width = ctx.measureText(str).width; 
+            fontSize-=4;
+        }
+        return fontSize + "px"; 
+    }
+
+
+
+
+    //The following is for Safari 8 and below
+    var ua = navigator.userAgent;
+    var verRe = /Version\/(\d+)/;
+    var version = ua.match(verRe);
+    var safari = ua.indexOf("Safari");
+
+    if (safari !== -1 && version !== null)  {
+        if (version[1] <= 8) {
+            addStyleTag("./css/safari8.css");
+        }
+    }
+
+    function addStyleTag(url) {
+        var link,head;
+        head = document.querySelector("head");
+        link = document.createElement("link");
+        link.setAttribute("href",url);
+        link.setAttribute("type","text/css");
+        link.setAttribute("rel","stylesheet");
+        head.appendChild(link);
+    }
+
+
+
+
+
+
+	function spawn() {
+        //var lastTime, minSpawnTime;
+        if(stopSmoking){return};
+		if (new Date().getTime() > lastTime + minSpawnTime) {
+			lastTime = new Date().getTime();
+			parts.push(new smoke(emitterX, emitterY));
+		}
+	}
+
+    
+    
+	function render() {
+		var len = parts.length;
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		while (len--) {
+			if (parts[len].y < 0 || parts[len].lifeTime > maxLifeTime) {
+				parts.splice(len, 1);
+			} else {
+				parts[len].update();
+				ctx.save();
+				var offsetX = -parts[len].size/2,offsetY = -parts[len].size/2;
+				ctx.translate(parts[len].x-offsetX, parts[len].y-offsetY);
+				ctx.rotate(parts[len].angle / 180 * Math.PI);
+				ctx.globalAlpha  = parts[len].alpha;
+				ctx.drawImage(smokeImage, offsetX,offsetY, parts[len].size, parts[len].size);
+				ctx.restore();
+			}
+		}
+		spawn();
+		requestAnimationFrame(render);
+	}
+
+	function smoke(x, y, index) {
+		this.x = x;
+		this.y = y;
+
+		this.size = 1;
+		this.startSize = 10;
+		this.endSize = 22;
+
+		this.angle = Math.random() * 359;
+
+		this.startLife = new Date().getTime();
+		this.lifeTime = 0;
+
+		this.velY = -1 - (Math.random()*0.04);
+		this.velX = Math.floor(Math.random() * (-6) + 3) / 10;
+	}
+
+	smoke.prototype.update = function () {
+		this.lifeTime = new Date().getTime() - this.startLife;
+		this.angle += 0.2;
+		
+		var lifePerc = ((this.lifeTime / maxLifeTime) * 100);
+
+		this.size = this.startSize + ((this.endSize - this.startSize) * lifePerc * .1);
+
+		this.alpha = 1 - (lifePerc * .01);
+		this.alpha = Math.max(this.alpha,0);
+		
+		this.x += this.velX;
+		this.y += this.velY;
+	}
+
+	//window.onresize = resizeMe;
+	//window.onload = resizeMe,calculateTotal();
+	//function resizeMe() {
+	   //canvas.height = document.body.offsetHeight;
+	//}
+	
+	 function preload(arrayOfImages) {
+		$(arrayOfImages).each(function () {
+			$('<img />').attr('src',this).appendTo('body').css('display','none');
+		});
+	}
+
+
+			
+	function createCookie(cookieName,CC){
+	 	$.cookie(cookieName,CC,{ expires : 7, path: '/' } );	
+	}
+
+
+
---
+++ js/1/utilities.js
@@ -0,0 +1,203 @@
+
+
+var consoleColorArr = [];
+
+
+function randomConsoleColor(){
+
+/*consoleColorArr[0] = "\x1b[30m";
+consoleColorArr[1] = "\x1b[31m";
+consoleColorArr[2] = "\x1b[32m";
+consoleColorArr[3] = "\x1b[33m";
+consoleColorArr[4] = "\x1b[34m";
+consoleColorArr[5] = "\x1b[35m";
+consoleColorArr[6] = "\x1b[36m";*/
+//consoleColorArr[7] = "\x1b[37m";
+consoleColorArr[0] = "\x1b[40m";
+consoleColorArr[1] = "\x1b[41m";
+consoleColorArr[2] = "\x1b[42m";
+consoleColorArr[3] = "\x1b[43m";
+consoleColorArr[4] = "\x1b[44m";
+consoleColorArr[5] = "\x1b[45m";
+consoleColorArr[6] = "\x1b[46m";
+consoleColorArr[7] = "\x1b[40m";
+consoleColorArr[8] = "\x1b[41m";
+consoleColorArr[8] = "\x1b[2m";
+consoleColorArr[10] = "\x1b[42m";
+consoleColorArr[11] = "\x1b[43m";
+consoleColorArr[12] = "\x1b[44m";
+consoleColorArr[13] = "\x1b[45m";
+consoleColorArr[14] = "\x1b[46m";
+consoleColorArr[15] = "\x1b[47m";
+consoleColorArr[16] = "\x1b[0m";
+consoleColorArr[17] = "\x1b[1m";
+consoleColorArr[18] = "\x1b[2m";
+consoleColorArr[19] = "\x1b[4m";
+consoleColorArr[20] = "\x1b[5m";
+consoleColorArr[21] = "\x1b[7m";
+consoleColorArr[22] = "\x1b[8m";
+
+var randomStrLen = [];
+randomStrLen[0] = "BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW ";
+randomStrLen[1] = "BUY NOW BUY NOW BUY NOW NOW BUY BUY NOW BUY NOW NOW BUY BUY NOW BUY BUY NOW BUY NOW NOW BUY BUY NOW BUY NOW NOW BUY NOW ";
+randomStrLen[2] = "BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY";
+randomStrLen[3] = "BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW";
+randomStrLen[4] = "BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY";
+randomStrLen[5] = "BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW BUY";
+randomStrLen[6] = "BUY BUY NOW BUY NOW NOW BUY BUY NOW BUY NOW NOW BUY BUY";
+randomStrLen[7] = "BUY NOW BUY NOW BUY NOW BUY NOW BUY NOW";
+randomStrLen[8] = "BUY NOW BUY NOW BUY NOW BUY";
+randomStrLen[9] = "BUY NOW BUY NOW";
+randomStrLen[10] = "BUY NOW";
+    
+    q = Math.floor((Math.random() * 15) + 1);
+    e = Math.floor((Math.random() * 10) + 1);
+    console.log(consoleColorArr[q] + "%s\x1b[0m" ,randomStrLen[e]);
+}
+
+
+
+
+
+        function getRandomColor() {
+			var letters = '0123456789ABCDEF';
+			var color = '#';
+			for (var i = 0; i < 6; i++) {
+			color += letters[Math.floor(Math.random() * 16)];
+			}
+			return color;
+		}
+		
+        var intervalID = 0;
+        var intervalID2 = 0;
+
+        function randomColors(onOrOff){
+        $("div").each(function(i) {
+                        rc = getRandomColor();
+                        if(this.id !== "nav-container"){
+                            onOrOff ? $(this).animate({backgroundColor: rc}, 'fast') : this.style.backgroundColor = "";
+                        }
+                    });
+        }
+
+
+	function fillHoles(holeCount,random = false){
+		
+		document.getElementById("svg-js").innerHTML = "";
+		var yCoo = 45, fillColor, circArr = [];
+		var draw = SVG('svg-js').size(70, 650);
+		draw.rect(70, 600).fill('#FFF').move(0,35);
+		draw.circle(70).fill('#FFF').move(0, 0);
+		draw.circle(70).fill('#FFF').move(0, 0);	
+		var i;
+        for(i = 0; i < 11; i++){
+            random ? randomOrNot = getRandomColor() : randomOrNot = "#5ba116";
+			(i<holeCount) ? fillColor = randomOrNot : fillColor = "#000000";
+			draw.circle(30).fill('' + fillColor + '').move(20, yCoo);
+			yCoo += 50;
+		}		
+	}
+
+
+    function strutAnimation(){
+    
+    
+    }
+
+
+		function divDebugMode(onOrOff = true){
+            
+            
+            if(onOrOff && intervalID2 == 0){
+            intervalID2 = window.setInterval(function(){
+                                   randomColors(onOrOff);
+                                }, 600);
+
+            }
+            
+            if(onOrOff && intervalID == 0){
+                    intervalID = window.setInterval(function(){
+                      randomConsoleColor();
+                    }, 80);            
+            }
+            if(!onOrOff){
+                clearInterval(intervalID2);
+                 intervalID2 = 0;
+                randomColors(false);
+              clearInterval(intervalID);
+            intervalID = 0;
+           
+                
+            console.clear();          
+            
+            }
+		}
+
+        var cartThumbnailArr = ["ibycus","pyramís","electrum"];
+        function showThumbnail(imgArrNum,show){
+            console.log("showThumbnail(" + imgArrNum + "," + show + ")");
+            //if(thumbnailHover){return};
+            $("#" + cartThumbnailArr[imgArrNum]).css("background-image", "url('/images/thumbs/" + cartThumbnailArr[imgArrNum] + ".png')");
+            if(show){
+                    $("#" + cartThumbnailArr[imgArrNum]).show(300);
+                }else{
+                    $("#" + cartThumbnailArr[imgArrNum]).hide(300);
+            }
+            
+        }
+
+        function decodeEntities(encodedString) {
+          var textArea = document.createElement('textarea');
+          textArea.innerHTML = encodedString;
+          return textArea.value;
+        }
+		
+		//get language code from querystring
+		function getLCfromQS(){
+			var vars = [], hash;
+				var q = document.URL.split('?')[1];
+				if(q == undefined){return false};
+				if(q != undefined){
+					q = q.split('&');
+					for(var i = 0; i < q.length; i++){
+						hash = q[i].split('=');
+						vars.push(hash[1]);
+						vars[hash[0]] = hash[1];
+					}
+			}
+			return vars['language'];
+		}
+		
+		function isDesbug(){
+			var vars = [], hash;
+				var q = document.URL.split('?')[1];
+				if(q == undefined){return false};
+				if(q != undefined){
+					q = q.split('&');
+					for(var i = 0; i < q.length; i++){
+						hash = q[i].split('=');
+						vars.push(hash[1]);
+						vars[hash[0]] = hash[1];
+					}
+			}
+			
+			//divDebugMode();
+			return vars['debug'];
+		}
+		
+        function setMeasurementVariables(){
+            $(".measurement-string").html(unitOfMeasure);
+            $(".standard-krane-length").html(sizeStandardKrane);
+            $(".jumbo-krane-length").html(sizeJumboKrane);
+            $(".length-symbol").html(lengthSymbol);
+        }		
+
+        function getParameterByName(name) {
+            var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+            return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+        }
+
+
+
+
+
